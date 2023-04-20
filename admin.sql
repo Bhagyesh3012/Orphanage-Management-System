d0f6b7c84082672ab9e6bf925fa38efbccc30ad4
68113_shashikant <badgujarshashikant007@gmail.com>
2023-04-20 15:19:20 +0530

    admin db added

---
+++ admin.sql
@@ -0,0 +1,15 @@
+create table admin (id INT not null primary key AUTO_INCREMENT, name varchar(20), designation varchar(20));
+
+insert into admin(name,designation) values("Mr.Sahara","Administrator"),("Mr.Narendra","Treasurer"),("Mr.Rohit","Inventory Manager"),("Mr.Kiran","clerk");
+
+
+
+
+-- +----+-------------+-------------------+
+-- | id | name        | designation       |
+-- +----+-------------+-------------------+
+-- |  1 | Mr.Sahara   | Administrator     |
+-- |  2 | Mr.Narendra | Treasurer         |
+-- |  3 | Mr.Rohit    | Inventory Manager |
+-- |  4 | Mr.Kiran    | clerk             |
+-- +----+-------------+-------------------+
